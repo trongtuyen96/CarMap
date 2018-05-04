3e9426edf87ce5c7e7be76f47b2e249336f002e5
Tuyển Nguyễn <Tuyn Nguyn>
2018-05-04 18:27:54 +0700

    A map with navigation drawer

---
+++ app/src/main/java/com/example/trongtuyen/carmap/MainActivity.kt
@@ -0,0 +1,387 @@
+package com.example.trongtuyen.carmap
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Location
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.*
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
+    val options = PolylineOptions()
+    private var mapReady = false
+    private var mapSetup = false
+    private var locationUpdateRunning = false
+    private var alreadyAskPermission = false
+    private var resumeFromRequestPermissionFail = false
+
+    // PlaceAutoCompleteFragment
+    private var placeAutoComplete: PlaceAutocompleteFragment? = null
+
+    companion object {
+        private const val CODE_REQUEST_PERMISSION_FOR_UPDATE_LOCATION = 1
+        private const val CODE_REQUEST_SETTING_FOR_UPDATE_LOCATION = 2
+        private const val CODE_REQUEST_PERMISSION_FOR_SETUP_MAP = 3
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Obtain placeAutoComplete fragment
+        placeAutoComplete = fragmentManager.findFragmentById(R.id.place_autocomplete) as PlaceAutocompleteFragment
+        placeAutoComplete!!.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+
+                Log.d("Maps", "Place selected: " + place.name)
+                addMarker(place);
+            }
+
+            override fun onError(status: Status) {
+                Log.d("Maps", "An error occurred: $status")
+            }
+        })
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, null, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                lastLocation = p0.lastLocation
+                //placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
+                options.add(LatLng(lastLocation.latitude, lastLocation.longitude))
+                if (mapReady) {
+                    map.addPolyline(options)
+                }
+            }
+        }
+        createLocationRequest()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Try catch parsing style json file to map
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            var success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json))
+
+            if (!success) {
+                Log.e("Resources", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("Resources", "Can't find style. Error: ", e)
+        }
+
+        mapReady = true
+
+        //map.uiSettings.isZoomControlsEnabled = true
+
+        map.setOnMarkerClickListener(this)
+
+        if (!mapSetup) {
+            setUpMapWrapper()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setUpMap() {
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                //placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                options.add(currentLatLng)
+            }
+        }
+
+        mapSetup = true
+    }
+
+    private fun setUpMapWrapper() {
+        if (!mapReady) return
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (!alreadyAskPermission){
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), CODE_REQUEST_PERMISSION_FOR_SETUP_MAP)
+                alreadyAskPermission = true
+            }
+            return
+        }
+        setUpMap()
+    }
+
+    override fun onMarkerClick(p0: Marker?) = false
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent()
+        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    fun addMarker(p: Place) {
+        val markerOptions = MarkerOptions()
+        markerOptions.position(p.latLng)
+        markerOptions.title(p.name.toString() + "")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+
+        map.addMarker(markerOptions)
+        map.moveCamera(CameraUpdateFactory.newLatLng(p.latLng))
+        map.animateCamera(CameraUpdateFactory.zoomTo(13f))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */)
+        locationUpdateRunning = true
+    }
+
+    private fun startLocationUpdatesWrapper() {
+        if (!mapSetup || locationUpdateRunning) return
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (!alreadyAskPermission) {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), CODE_REQUEST_PERMISSION_FOR_UPDATE_LOCATION)
+                alreadyAskPermission = true
+            }
+            return
+        }
+        startLocationUpdates()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun createLocationRequest() {
+        // 1
+        locationRequest = LocationRequest()
+        // 2
+        locationRequest.interval = 10000
+        // 3
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+
+        // 4
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        // 5
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            //startLocationUpdatesWrapper()
+        }
+        task.addOnFailureListener { e ->
+            // 6
+            if (e is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    e.startResolutionForResult(this@MainActivity,
+                            CODE_REQUEST_SETTING_FOR_UPDATE_LOCATION)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        //super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CODE_REQUEST_SETTING_FOR_UPDATE_LOCATION) {
+            if (resultCode == Activity.RESULT_OK) {
+                locationUpdateState = true
+                //startLocationUpdatesWrapper()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //Toast.makeText(this,"On Pause",Toast.LENGTH_SHORT).show()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        locationUpdateRunning = false
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        //Toast.makeText(this,"On Stop",Toast.LENGTH_SHORT).show()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        //Toast.makeText(this,"On Resume",Toast.LENGTH_SHORT).show()
+        if (!mapSetup && !resumeFromRequestPermissionFail) {
+            setUpMapWrapper()
+        }
+        if (mapSetup && locationUpdateState && !locationUpdateRunning&& !resumeFromRequestPermissionFail) {
+            startLocationUpdatesWrapper()
+        }
+        resumeFromRequestPermissionFail=false
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CODE_REQUEST_PERMISSION_FOR_UPDATE_LOCATION -> {
+                alreadyAskPermission = false
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    startLocationUpdates()
+                } else {
+
+                    //Toast.makeText(this, "UPDATE LOCATION DENIED", Toast.LENGTH_LONG).show()
+                    //Log.e("K", "UPDATE LOCATION DENIED")
+                    resumeFromRequestPermissionFail = true;
+                    openAppSettings()
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            CODE_REQUEST_PERMISSION_FOR_SETUP_MAP -> {
+                alreadyAskPermission = false
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    setUpMap()
+                } else {
+                    //Toast.makeText(this, "SETUP MAP DENIED", Toast.LENGTH_LONG).show()
+                    //Log.e("K", "SETUP MAP DENIED")
+                    resumeFromRequestPermissionFail = true;
+                    openAppSettings()
+
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+        // Add other 'when' lines to check for other
+        // permissions this app might request.
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+}
