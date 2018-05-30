eaa528c3bbdddefa9a03c2b9bcabde2a3affc72d
Tuyển Nguyễn <Tuyn Nguyn>
2018-05-30 15:44:46 +0700

    Add get nearby user

--- app/src/main/java/com/example/trongtuyen/carmap/services/UserService.kt
+++ app/src/main/java/com/example/trongtuyen/carmap/services/UserService.kt
@@ -1,11 +1,13 @@
 package com.example.trongtuyen.carmap.services
 
 import com.example.trongtuyen.carmap.models.User
+import com.example.trongtuyen.carmap.services.models.NearbyReportsResponse
 import com.example.trongtuyen.carmap.services.models.UserProfileResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PUT
+import retrofit2.http.Query
 
 /**
  * Created by tuyen on 08/05/2018.
@@ -21,8 +23,11 @@ interface UserService {
     // @FormUrlEncoded
     // Bỏ FormUrlEncoded vì gây lỗi với @Body
     @PUT("user/updateHomeLocation")
-    abstract fun updateHomeLocation(@Body user: User): Call<UserProfileResponse>
+    fun updateHomeLocation(@Body user: User): Call<UserProfileResponse>
 
     @PUT("user/updateSocketID")
-    abstract fun updateSocketID(@Body user: User): Call<UserProfileResponse>
+    fun updateSocketID(@Body user: User): Call<UserProfileResponse>
+
+    @GET("user/nearby")
+    fun getNearbyUsers(@Query("lat") lat: Double?, @Query("lng") lng: Double?, @Query("radius") radius: Float): Call<List<User>>
 }
