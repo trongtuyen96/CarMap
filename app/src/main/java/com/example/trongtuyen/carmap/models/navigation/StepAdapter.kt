928d4ba59cb518c0f24addd4d63bbf19fcd4833b
Tuyển Nguyễn <trongtuyen96@gmail.com>
2018-07-19 23:58:14 +0700

    Add maneuvers

--- app/src/main/java/com/example/trongtuyen/carmap/models/navigation/StepAdapter.kt
+++ app/src/main/java/com/example/trongtuyen/carmap/models/navigation/StepAdapter.kt
@@ -52,6 +52,68 @@ class StepAdapter(private val myStepSet: ArrayList<Step>) :
     val tvDistance=holder.view.findViewById<TextView>(R.id.tvDistance_step_adapter)
     tvInstruction.text = myStepSet[position].instruction
     tvDistance.text = myStepSet[position].distance!!.text
+    when (myStepSet[position].maneuver) {
+        "ferry" -> {
+            imInstruction.setImageResource(R.drawable.ferry)
+        }
+        "ferry-train" -> {
+            imInstruction.setImageResource(R.drawable.ferry_train)
+        }
+        "fork-left" -> {
+            imInstruction.setImageResource(R.drawable.fork_left)
+        }
+        "fork-right" -> {
+            imInstruction.setImageResource(R.drawable.fork_right)
+        }
+        "keep-left" -> {
+            imInstruction.setImageResource(R.drawable.keep_left)
+        }
+        "keep-right" -> {
+            imInstruction.setImageResource(R.drawable.keep_right)
+        }
+        "merge" -> {
+            imInstruction.setImageResource(R.drawable.merge)
+        }
+        "ramp-left" -> {
+            imInstruction.setImageResource(R.drawable.ramp_left)
+        }
+        "ramp-right" -> {
+            imInstruction.setImageResource(R.drawable.ramp_right)
+        }
+        "roundabout-left" -> {
+            imInstruction.setImageResource(R.drawable.roundabout_left)
+        }
+        "roundabout-right" -> {
+            imInstruction.setImageResource(R.drawable.roundabout_right)
+        }
+        "straight" -> {
+            imInstruction.setImageResource(R.drawable.straight)
+        }
+        "turn-left" -> {
+            imInstruction.setImageResource(R.drawable.turn_left)
+        }
+        "turn-right" -> {
+            imInstruction.setImageResource(R.drawable.turn_right)
+        }
+        "turn-sharp-left" -> {
+            imInstruction.setImageResource(R.drawable.turn_sharp_left)
+        }
+        "turn-sharp-right" -> {
+            imInstruction.setImageResource(R.drawable.turn_sharp_right)
+        }
+        "turn-slight-left" -> {
+            imInstruction.setImageResource(R.drawable.turn_slight_left)
+        }
+        "turn-slight-right" -> {
+            imInstruction.setImageResource(R.drawable.turn_slight_right)
+        }
+        "uturn-left" -> {
+            imInstruction.setImageResource(R.drawable.uturn_left)
+        }
+        "uturn-right" -> {
+            imInstruction.setImageResource(R.drawable.uturn_right)
+        }
+    }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
